78491f470fae67de49c0ba20cc9b216b935c623c
AIWorkers11 <devfairy529@gmail.com>
2022-12-19 12:02:44 +0900

    移動・送信ボタン追加

--- src/console.py
+++ src/console.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-import tkinter.ttk as ttk
 from PIL import ImageTk
+import threading 
+import string
 from sprigatito import Sprigatito
 
 class console:
@@ -13,6 +14,10 @@ class console:
         self.frame1 = tk.Frame(self.base,relief=tk.RAISED,bd=1,padx=10,pady=10)
         self.exitbutton = tk.Button(self.frame1,width=10,height=5,text="終了",command=lambda:self.base.destroy())
         self.exitbutton.pack()
+        self.movebutton = tk.Button(self.frame1,width=10,height=5,text="上に移動\n（デモ）",command=lambda:self.moveup())
+        self.movebutton.pack()
+        self.pushbutton = tk.Button(self.frame1,width=10,height=5,text="送信",command=lambda:self.push())
+        self.pushbutton.pack(side=tk.BOTTOM)
         self.frame1.pack(side=tk.LEFT,fill=tk.BOTH)
 
         self.frame2 = tk.Frame(self.base,relief=tk.RAISED,bd=1,padx=10,pady=10)
@@ -24,16 +29,37 @@ class console:
         self.Sprigatito.setModelPosX(425) #初期値：425（中央）
         self.Sprigatito.setModelPosY(250) #初期値：250（中央）
 
-        self.canvas.create_image(
+        self.imgid = self.canvas.create_image(
             self.Sprigatito.getModelPosX(),
             self.Sprigatito.getModelPosY(),
             image=self.Sprigatitoimage
         )        
+        self.outlabel = tk.Label(self.frame2,text="出力")
         self.outputbox = tk.Text(self.frame2,width=10,height=7)
         self.outputbox.configure(state="disabled")
-        self.inputbox = tk.Text(self.frame2,width=10,height=3)
+        self.inlabel = tk.Label(self.frame2,text="入力")
+        self.inputbox = tk.Text(self.frame2,width=10,height=1)
 
         self.canvas.pack()
+        self.outlabel.pack()
         self.outputbox.pack(fill=tk.BOTH)
+        self.inlabel.pack()
         self.inputbox.pack(fill=tk.BOTH)
         self.frame2.pack(side=tk.BOTTOM,fill=tk.BOTH)
+
+    def moveup(self):
+        self.Sprigatito.setModelPosY(self.Sprigatito.getModelPosY()-10)
+        print(self.Sprigatito.getModelPosY())
+        self.canvas.move(self.imgid,0,-10)
+
+
+    def push(self):
+        text = self.inputbox.get("1.0","end")
+        if not text.isspace():
+            text = text.replace("\n","")
+            self.inputbox.delete("1.0","end")
+            self.outputbox.configure(state="normal")
+            self.outputbox.insert("end",text+"\n")
+            self.outputbox.configure(state="disabled")
+
+        
